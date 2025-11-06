d1bc84d7a77c43e83db43a3fc3bc7e4b2f4b89eb
okmansyah <59006646+okmansyah@users.noreply.github.com>
2025-11-06 08:36:53 +0700

    Fix admin password retrieval for book return

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const urlAnggota = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQT5Drx7hO3X54afpQyQEj01DTXQLON2eAAG5OIBjNL24Ub_6pIJ6Sr43gjQKAkd_J3nrHfM1XrhNI-/pub?gid=485044064&single=true&output=csv'; 
 
     // 2. LINK BACKEND (UNTUK TULIS DATA / CRUD)
-    const urlWebApp = 'https://script.google.com/macros/s/AKfycbyeoIbVg-ZSuDKk-6zweHTlXSjEMbsXBx2GP5XdqJNQyBGc5EzlsLCf0Ick-5BgOMP5/exec';
+    const urlWebApp = 'https://script.google.com/macros/s/AKfycbwM1VqMNd4WbL36c1aXNO6KfI3Vz0a459I22UX2NjJZck-UVRqssIeEKK6opOImPF9R/exec';
 
     // ==========================================================
     // == Variabel Global & Elemen ==
@@ -304,3 +304,4 @@ document.addEventListener("DOMContentLoaded", function() {
     muatDataBuku();
     muatDataAnggota();
 });
+
